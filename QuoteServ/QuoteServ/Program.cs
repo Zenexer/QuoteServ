64e7bc9a6b34e44241e4d4303a52849f5cf6660a
Paul Buonopane <paul@earth2me.com>
2012-10-05 13:52:26 -0400

    Extreneous solution/project files.
    Signed-off-by: Paul Buonopane <paul@earth2me.com>

---
+++ QuoteServ/QuoteServ/Program.cs
@@ -0,0 +1,22 @@
+using System;
+
+
+namespace QuoteServ
+{
+	/// <summary>
+	/// Contains the main entry point for the application.
+	/// </summary>
+	public class Program
+	{
+		/// <summary>
+		/// The entry point of the program, where the program control starts and ends.
+		/// </summary>
+		/// <param name='args'>
+		/// The command-line arguments.
+		/// </param>
+		[MTAThread]
+		public static void Main(string[] args)
+		{
+		}
+	}
+}
